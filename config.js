e035f23e7158106d2f7765e2c30cdf074e99345c
PRATIK-RAKTATE <rhutikraktate@gmail.com>
2026-03-27 08:14:57 +0530

    change config url to intergo.netlify.app for testing purpose

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 export const CONFIG = {
-  URL: "https://onlineresults.unipune.ac.in/Result/Dashboard/Default",
+  URL: "https://intergo.netlify.app/",
   INTERVAL: "*/10 * * * *", // every 10 min
   TIMEOUT: 10000
 };
